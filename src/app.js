301641ea1ddfc58c7746813dc0ab1b58769aa455
reazulislam1487 <reazulislam1487@gmail.com>
2026-01-08 16:31:39 +0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+
+import authRoutes from "./modules/auth/auth.routes.js";
+import urlRoutes from "./modules/url/url.routes.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
+import Url from "./models/Url.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/urls", urlRoutes);
+
+/**
+ * Redirect + click tracking
+ */
+app.get("/:shortCode", async (req, res, next) => {
+  try {
+    const url = await Url.findOne({ shortCode: req.params.shortCode });
+    if (!url) return res.status(404).send("Not Found");
+
+    url.clicks += 1;
+    await url.save();
+
+    res.redirect(url.originalUrl);
+  } catch (err) {
+    next(err);
+  }
+});
+
+app.use(errorHandler);
+
+export default app;
